f3db120c8ef0e2dd60e099c5d4c60d0812394cd9
Maira Galvao <mairagalvao01@gmail.com>
2021-05-27 22:23:33 +0300

    redirect to /health with spaces adjusted

--- public/script.js
+++ public/script.js
@@ -2,6 +2,9 @@ const getData=()=>{
     // upon click, redirect to /health. 
     // this will call the relevant route in the server, which will do an API call to Nambeu.
     // the results will be shown on the /health route
-    location.href = 'http://localhost:3000/health?city=tel%20aviv&country=israel'
-    
+    let cityUser = document.getElementById('city').value
+    let countryUser = document.getElementById('country').value
+    cityUser = cityUser.split(' ').join('+');
+    countryUser = countryUser.split(' ').join('+');
+    location.href = `http://localhost:3000/health?city=${cityUser}&country=${countryUser}`
 }
