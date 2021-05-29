2b6271a267267349a1d9907e7afa8e4fb4487404
Maira Galvao <mairagalvao01@gmail.com>
2021-05-29 20:59:47 +0300

    project done

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
-const express = require('express')
-const app = express()
-const accessKey = '3yhz4zai60zzxn';
-const fetch = require('node-fetch');
-const port = 3000
-const path = require('path');
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+const port = 3000;
+const path = require("path");
+require("dotenv").config();
+const accessKey = process.env.NUMBEO_KEY;
 
 const cityRowData = async function (country, city) {
-    return new Promise((resolve, reject) => {
-        const urlCity = `https://www.numbeo.com/api/city_healthcare?api_key=${accessKey}&query=${city}, ${country}`;
-        const res = fetch(urlCity).then(res => res.json())
-        resolve(res)
-    }) //when we have time, understand if there is a need for the code above
-}
+  return new Promise((resolve, reject) => {
+    const urlCity = `https://www.numbeo.com/api/city_healthcare?api_key=${accessKey}&query=${city}, ${country}`;
+    const res = fetch(urlCity).then((res) => res.json());
+    resolve(res);
+  }); //when we have time, understand if there is a need for the code above
+};
 const fetchData = async function (country, city) {
-    const result = await cityRowData(country, city)
-    return (result)
-}
+  const result = await cityRowData(country, city);
+  return result;
+};
 
-app.use('/home',express.static(__dirname+'/public')) 
+app.use("/home", express.static(__dirname + "/public"));
 //about page contains just a welcome and an explanation of the website
 //it serves the html to the local host page in the route home
 
-app.get('/health', (req, res) => {
-    console.log(req.query)
-    const cityUser = req.query.city
-    const countryUser = req.query.country
-    if (!cityUser || !countryUser) {
-        return
-    }
-    fetchData(countryUser, cityUser).then(jsonData => res.send(jsonData))
-})
-
-
-
+app.get("/health", (req, res) => {
+  console.log(req.query);
+  const cityUser = req.query.city;
+  const countryUser = req.query.country;
+  if (!cityUser || !countryUser) {
+    return;
+  }
+  fetchData(countryUser, cityUser).then((jsonData) => res.send(jsonData));
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
